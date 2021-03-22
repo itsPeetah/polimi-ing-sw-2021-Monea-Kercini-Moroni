844dd532e2cd85a9f9d3a3dc40c8833e589edf0b
itsPeetah <pietro.g.moroni@gmail.com>
2021-03-22 16:15:36 +0100

    Added classes Player MarketTray And Game

---
+++ src/main/java/it/polimi/ingsw/module/game/Game.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model.game;
+
+public class Game {
+    private static final int MAX_PLAYERS = 4;
+
+    // Players
+
+    private MarketTray tray;
+
+    public Game(){
+
+
+    }
+
+}
