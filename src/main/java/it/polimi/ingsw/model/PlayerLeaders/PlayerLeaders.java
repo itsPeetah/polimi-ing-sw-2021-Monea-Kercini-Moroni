11a51f7b59f6bf4f2769e234bd8ecea9f127dcc0
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-03-26 14:17:23 +0100

    Added method getPlayableCards,
    which returns all cards that are in hand. (They might still be unpurchasable because of lack of resources.)

--- src/main/java/it/polimi/ingsw/model/PlayerLeaders/PlayerLeaders.java
+++ src/main/java/it/polimi/ingsw/model/PlayerLeaders/PlayerLeaders.java
@@ -1,25 +1,32 @@
 package it.polimi.ingsw.model.PlayerLeaders;
 
 import it.polimi.ingsw.model.cards.LeadCard;
+import it.polimi.ingsw.model.general.Production;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class PlayerLeaders {
 
     private LeadCard[] cards;
     private CardState[] cardStates;
 
-    /** da finire
 
-    public LeadCard[] getPlayableCards(){
-        LeadCard[] playableCards;
+
+    public ArrayList<LeadCard> getPlayableCards(){
+
+        ArrayList<LeadCard> playableCards = new ArrayList<LeadCard>();
 
         for(int i = 0; i < cards.length; i++){
-            if (cardStates[i] == CardState.INHAND);
+            if (cardStates[i] == CardState.INHAND){
+                playableCards.add(cards[i]);
+            }
         }
+
+        return playableCards;
     }
 
-     */
+
 
     public void playCard(int index){
         cardStates[index] = CardState.PLAYED;
