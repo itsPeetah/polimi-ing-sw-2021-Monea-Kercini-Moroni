0148566f75f6c290027048ea5af63da56e781317
itsPeetah <pietro.g.moroni@gmail.com>
2021-03-26 09:13:31 +0100

    Started updating Game class

--- src/main/java/it/polimi/ingsw/model/game/Game.java
+++ src/main/java/it/polimi/ingsw/model/game/Game.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.model.game;
 
+import java.util.ArrayList;
+
 public class Game {
     private static final int MAX_PLAYERS = 4;
+    private static final int MAX_VATICAN_REPORTS = 3;
 
     private Integer turnCounter;
     private Integer lastVaticanReport;
@@ -9,4 +12,23 @@ public class Game {
     private MarketTray resourceMarket;
     private DevCardMarket devCardMarket;
 
+    private ArrayList<Player> players;
+
+    /**
+     * Gets the array players in the game.
+     * @return The players.
+     */
+    public Player[] getPlayers(){
+        Player[] pls = new Player[players.size()];
+        return players.toArray(pls);
+    }
+
+    /**
+     * Gets the player whose turn it currently is.
+     * @return The current player.
+     */
+    public Player getCurrentPlayer(){
+        int currentPlayer = turnCounter%players.size();
+        return  players.get(currentPlayer);
+    }
 }
