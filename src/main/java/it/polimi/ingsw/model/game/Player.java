b9221e716879c31e3dc21b426305045241401c3a
itsPeetah <pietro.g.moroni@gmail.com>
2021-03-26 09:12:56 +0100

    Started adding Player class

---
+++ src/main/java/it/polimi/ingsw/model/game/Player.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.model.game;
+
+import it.polimi.ingsw.model.PlayerBoard.PlayerBoard;
+
+public class Player {
+
+    private PlayerBoard board;
+    // private PlayerLeaders leaders;
+
+    public PlayerBoard getBoard() {return  board;}
+
+}
