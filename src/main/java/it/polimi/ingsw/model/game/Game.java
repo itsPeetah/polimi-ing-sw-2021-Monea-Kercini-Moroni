02f36afc57fda7006b46c526c0cd83b271611506
itsPeetah <pietro.g.moroni@gmail.com>
2021-04-03 10:47:08 +0200

    Update game class

--- src/main/java/it/polimi/ingsw/model/game/Game.java
+++ src/main/java/it/polimi/ingsw/model/game/Game.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model.game;
 
+import it.polimi.ingsw.model.playerboard.PlayerBoard;
+
 import java.util.ArrayList;
 
 public class Game {
@@ -17,42 +19,110 @@ public class Game {
     /**
      * Constructor for the class.
      */
-    public Game(){
+    public Game() {
         turnCounter = 0;
         lastVaticanReport = 0;
 
-        // TODO implement game settings
+        // TODO: Implement game settings
         // resourceMarket = new MarketTray(rows, cols, marbles);
         // devCardMarket = new DevCardMarket(cards);
 
         players = new ArrayList<Player>();
     }
 
+    // Player handling ------------------------------------------------------------------
+
     /**
      * Add a new player to the game.
+     *
      * @param nickname Nickname of the player.
      * @throws GameException if the player can't be added to the game.
      */
-    public void addPlayer(String nickname) throws  GameException
-    {
+    public void addPlayer(String nickname) throws GameException {
         // Check if player limit has already been reached
-        if(players.size() >= MAX_PLAYERS) throw new GameException("Too many players in the game. Unable to join.");
+        if (players.size() >= MAX_PLAYERS) throw new GameException("Too many players in the game. Unable to join.");
         // Create and register the new player
         Player newPlayer = new Player(nickname);
         players.add(newPlayer);
     }
+
     /**
      * Gets the array players in the game.
      */
-    public Player[] getPlayers(){
+    public Player[] getPlayers() {
         Player[] pls = new Player[players.size()];
         return players.toArray(pls);
     }
+
     /**
      * Gets the player whose turn it currently is.
      */
-    public Player getCurrentPlayer(){
-        int currentPlayer = turnCounter%players.size();
-        return  players.get(currentPlayer);
+    public Player getCurrentPlayer() {
+        int currentPlayer = turnCounter % players.size();
+        return players.get(currentPlayer);
     }
+
+    // Component getters ------------------------------------------------------------------
+
+    /**
+     * Resource Market getter.
+     */
+    public MarketTray getResourceMarket() {
+        return resourceMarket;
+    }
+
+    /**
+     * Development Card Market getter.
+     */
+    public DevCardMarket getDevCardMarket() {
+        return devCardMarket;
+    }
+
+    // Data getters ------------------------------------------------------------------
+
+    /**
+     * Turn counter getter.
+     */
+    public Integer getTurnCounter() {
+        return turnCounter;
+    }
+
+    /**
+     * Vatican report getter.
+     */
+    public Integer getLastVaticanReport() {
+        return lastVaticanReport;
+    }
+
+    /**
+     * Let all eligible players attend a vatican report.
+     *
+     * @throws GameException if the report can't be attended
+     */
+    public void DoVaticanReport() throws GameException {
+        if (lastVaticanReport >= MAX_VATICAN_REPORTS) throw new GameException("Max vatican records already performed.");
+
+        int currentReport = lastVaticanReport + 1;
+
+        // TODO Add customization?
+        // Vatican report range?
+        // for now hardcoded
+        for (Player p : players) {
+            boolean eligible;
+            int playersFaithPoints = 0; // TODO Add logic to get player's FPs
+            // Check eligibility
+            if (currentReport == 1 && playersFaithPoints >= 5 && playersFaithPoints <= 8)
+                eligible = true;
+            else if (currentReport == 2 && playersFaithPoints >= 12 && playersFaithPoints <= 16)
+                eligible = true;
+            else if (currentReport == 3 && playersFaithPoints >= 19 && playersFaithPoints <= 24)
+                eligible = true;
+            else eligible = false;
+
+            if (eligible) p.getBoard().attendReport(currentReport);
+        }
+        // Increase VR counter
+        lastVaticanReport++;
+    }
+
 }
