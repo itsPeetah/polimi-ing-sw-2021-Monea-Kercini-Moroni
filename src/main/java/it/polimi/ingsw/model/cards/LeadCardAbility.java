dfdbc54f88b090008196dc3cf455cebe5759342c
giommok <giovanni.monea@epfl.ch>
2021-04-03 17:52:25 +0200

    Started LeadCardAbility class

--- src/main/java/it/polimi/ingsw/model/cards/LeadCardAbility.java
+++ src/main/java/it/polimi/ingsw/model/cards/LeadCardAbility.java
@@ -1,4 +1,22 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.model.general.Resources;
+
 public class LeadCardAbility {
+    private Resources resourceDiscount;
+    /*
+    -resourceDiscount : Resources
+    -extraWarehouseSpace : Resources
+    -greyMarbleReplacement : Resources
+    -production : Production
+     */
+
+
+    /*
+
+    +getResourceDiscount()
+    +getExtraWarehouseSpace()
+    +getGreyMarbleReplacement()
+    +getProduction()
+     */
 }
