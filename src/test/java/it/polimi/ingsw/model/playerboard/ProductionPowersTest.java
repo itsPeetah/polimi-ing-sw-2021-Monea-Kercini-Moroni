280591b4d1156290208afb67be7bcda333149107
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-04-07 00:09:59 +0200

    Added Constructor for Production

--- src/test/java/it/polimi/ingsw/model/playerboard/ProductionPowersTest.java
+++ src/test/java/it/polimi/ingsw/model/playerboard/ProductionPowersTest.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.model.playerboard;
 
+import it.polimi.ingsw.model.cards.DevCard;
+import it.polimi.ingsw.model.general.Level;
+import it.polimi.ingsw.model.general.ResourceType;
+import it.polimi.ingsw.model.general.Resources;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +12,7 @@ class ProductionPowersTest {
 
     @Test
     void getAvailableProductions() {
+
     }
 
     @Test
@@ -16,6 +21,11 @@ class ProductionPowersTest {
 
     @Test
     void addDevCard() {
+        Resources cost = new Resources();
+        Resources in = new Resources();
+        in.add(ResourceType.STONES, 3);
+        out.add(ResourceType.SERVANTS, 2);
+        DevCard dc = new DevCard(3, 1, Level.LOW, cost,  );
     }
 
     @Test
