30f18b1af9a9c839f91608ee7b09a973c352be7c
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-04-07 00:27:28 +0200

    Testing in ProductionPowers

--- src/test/java/it/polimi/ingsw/model/playerboard/ProductionPowersTest.java
+++ src/test/java/it/polimi/ingsw/model/playerboard/ProductionPowersTest.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.model.playerboard;
 
 import it.polimi.ingsw.model.cards.DevCard;
-import it.polimi.ingsw.model.general.Level;
-import it.polimi.ingsw.model.general.ResourceType;
-import it.polimi.ingsw.model.general.Resources;
+import it.polimi.ingsw.model.general.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,13 +20,33 @@ class ProductionPowersTest {
     }
 
     @Test
-    void addDevCard() {
+    void DevCards_Productions() {
         Resources cost = new Resources();
         Resources in = new Resources();
         Resources out = new Resources();
         in.add(ResourceType.STONES, 3);
         out.add(ResourceType.SERVANTS, 2);
-        //DevCard dc = new DevCard(3, 1, Level.LOW, cost,  );
+        Production prod = new Production(in, out);
+        Production prod2 = new Production(out, in);
+        DevCard dc = new DevCard(3, "a", Level.LOW, Color.BLUE, cost, prod);
+        DevCard dc2 = new DevCard(3, "b", Level.LOW, Color.RED, cost, prod);
+        //Initialized a fake DevCard
+
+        ProductionPowers pp = new ProductionPowers(3);
+
+        pp.addDevCard(dc, 0);
+
+        ArrayList<DevCard> AvailableCards = new ArrayList<DevCard>();
+        AvailableCards.add(dc);
+
+        assertEquals(pp.getOwnedDevCards(), AvailableCards);
+
+        AvailableCards.add(dc2);
+        pp.addDevCard(dc2, 2);
+
+        assertEquals(pp.getOwnedDevCards(), AvailableCards);
+
+
     }
 
     @Test
