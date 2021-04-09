a889f412d91a4e37cb3a6a9332d869bb0b84b715
Giovanni Monea <giovanni.monea@epfl.ch>
2021-04-09 14:22:36 +0200

    Modified devcard template

--- src/test/java/it/polimi/ingsw/model/cards/CardManagerTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/CardManagerTest.java
@@ -1,15 +1,8 @@
 package it.polimi.ingsw.model.cards;
 
-import com.google.gson.Gson;
-import it.polimi.ingsw.model.general.Color;
-import it.polimi.ingsw.model.general.Level;
-import it.polimi.ingsw.model.general.Production;
-import it.polimi.ingsw.model.general.Resources;
+import it.polimi.ingsw.model.general.*;
 import org.junit.Test;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,12 +10,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CardManagerTest {
 
     @Test
-    public void loadDevCardFromJsonTest() throws IOException {
+    public void loadDevCardFromJsonTest() {
         /*
         //Generating devcard
 
         Gson gson = new Gson();
         Resources expRes = new Resources();
+        expRes.add(ResourceType.SHIELDS, 3);
         Level expLev = Level.values()[0];
         Color expCol = Color.values()[0];
 
@@ -34,6 +28,15 @@ public class CardManagerTest {
         file.flush();
         file.close();*/
 
-        assertFalse(CardManager.loadDevCardFromJson().isEmpty());
+        ArrayList<DevCard> cards = CardManager.loadDevCardFromJson();
+        assertFalse(cards.isEmpty());
+        Resources expRes = new Resources();
+        expRes.add(ResourceType.SHIELDS, 3);
+        Level expLev = Level.values()[0];
+        Color expCol = Color.values()[0];
+
+        DevCard devCard = new DevCard(0, "0", expLev, expCol, expRes, new Production(expRes, expRes));
+        assertEquals(devCard.getVictoryPoints(), cards.get(0).getVictoryPoints());
+        System.out.println(cards.get(0));
     }
 }
