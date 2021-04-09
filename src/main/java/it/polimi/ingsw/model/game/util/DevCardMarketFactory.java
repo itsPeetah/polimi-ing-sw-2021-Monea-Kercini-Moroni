37c2e7aa499ad57dc1b6ffbe336ba92ed10cec6a
itsPeetah <pietro.g.moroni@gmail.com>
2021-04-09 10:15:50 +0200

    Updated DevCardMarket factory, still needs implementing (card creation).

--- src/main/java/it/polimi/ingsw/model/game/util/DevCardMarketFactory.java
+++ src/main/java/it/polimi/ingsw/model/game/util/DevCardMarketFactory.java
@@ -1,12 +1,15 @@
 package it.polimi.ingsw.model.game.util;
 
+import it.polimi.ingsw.model.cards.DevCard;
 import it.polimi.ingsw.model.game.DevCardMarket;
 
 public class DevCardMarketFactory {
 
     public static DevCardMarket BuildDevCardMarket(GameSettingsLevel cardAmount){
 
-        return null;
+        DevCard[] cards = null;
+        DevCardMarket dcm = new DevCardMarket(cards);
+        return dcm;
     }
 
 }
