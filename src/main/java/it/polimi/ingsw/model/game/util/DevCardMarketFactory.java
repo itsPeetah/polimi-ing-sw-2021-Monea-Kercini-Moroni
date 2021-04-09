6bacdb75332f2713d213074c353e52a72f75a938
itsPeetah <pietro.g.moroni@gmail.com>
2021-04-09 16:10:08 +0200

    Implemented the card manager in the DevCardMarket factory.

--- src/main/java/it/polimi/ingsw/model/game/util/DevCardMarketFactory.java
+++ src/main/java/it/polimi/ingsw/model/game/util/DevCardMarketFactory.java
@@ -1,13 +1,16 @@
 package it.polimi.ingsw.model.game.util;
 
+import it.polimi.ingsw.model.cards.CardManager;
 import it.polimi.ingsw.model.cards.DevCard;
 import it.polimi.ingsw.model.game.DevCardMarket;
+
+import java.util.ArrayList;
 
 public class DevCardMarketFactory {
 
     public static DevCardMarket BuildDevCardMarket(GameSettingsLevel cardAmount){
 
-        DevCard[] cards = new DevCard[0];
+        ArrayList<DevCard> cards = CardManager.loadDevCardsFromJson();
         DevCardMarket dcm = new DevCardMarket(cards);
         return dcm;
     }
