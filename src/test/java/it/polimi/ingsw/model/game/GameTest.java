4d96f28480611a7096415aa9029e43399c6f5ff0
itsPeetah <pietro.g.moroni@gmail.com>
2021-04-09 10:19:38 +0200

    Added game test

---
+++ src/test/java/it/polimi/ingsw/model/game/GameTest.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.model.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void addPlayer() {
+    }
+
+    @Test
+    void getPlayers() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getResourceMarket() {
+    }
+
+    @Test
+    void getDevCardMarket() {
+    }
+
+    @Test
+    void getTurnCounter() {
+    }
+
+    @Test
+    void getLastVaticanReport() {
+    }
+
+    @Test
+    void doVaticanReport() {
+    }
+}
