598af1922c2610bc4b1a6e5ac852f79b576e2bae
itsPeetah <pietro.g.moroni@gmail.com>
2021-04-12 12:43:08 +0200

    Updated Game factory class.

--- src/main/java/it/polimi/ingsw/model/game/util/GameFactory.java
+++ src/main/java/it/polimi/ingsw/model/game/util/GameFactory.java
@@ -8,16 +8,10 @@ import java.util.ArrayList;
 
 public class GameFactory {
 
-    /**
-     * Custom game factory method.
-     * @param resources Amount of resources in the market tray and market tray size.
-     * @param devCards Amount of dev cards on the board.
-     * @return The game instance
-     */
-    public static Game CreateGame(GameSettingsLevel resources, GameSettingsLevel devCards){
-        MarketTray mt = MarketTrayFactory.BuildMarketTray(resources);
-        DevCardMarket dcm = DevCardMarketFactory.BuildDevCardMarket(devCards);
-        return new Game(mt,dcm);
+    public static Game CreateGame(GameCustomizationSettings settings){
+        MarketTray mt = MarketTrayFactory.BuildMarketTray(settings.getMarketTrayLevel());
+        DevCardMarket dcm = DevCardMarketFactory.BuildDevCardMarket(settings.getDevCardMarketLevel());
+        return new Game(mt, dcm);
     }
 
 
