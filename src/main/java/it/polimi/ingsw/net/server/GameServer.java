6f5a484b88bad7ef68e05712bf36af6ee59fefe5
itsPeetah <pietro.g.moroni@gmail.com>
2021-04-23 16:54:16 +0200

    Updated game server class

--- src/main/java/it/polimi/ingsw/net/server/GameServer.java
+++ src/main/java/it/polimi/ingsw/net/server/GameServer.java
@@ -9,35 +9,70 @@ import java.util.concurrent.Executors;
 
 public class GameServer {
 
-    private final String hostName;
-    private final int portNumber;
-    private ServerSocket serverSocket;
+    // Class singleton instance
+    private static GameServer instance;
 
-    private HashMap<String, ServerSideClient> activeClients;
+    private final String hostName;  // Server host-name
+    private final int portNumber;   // Server port-number
+    private ServerSocket serverSocket;  // Server socket
 
+    private Object clientMapLock;   // Lock for threads to access the activeClients map
+    private HashMap<String, ServerSideClient> activeClients;    // map with the active clients
+
+    /**
+     * Class constructor.
+     * @param hostName Server address.
+     * @param portNumber Server port.
+     */
     public GameServer(String hostName, int portNumber){
         this.hostName = hostName;
         this.portNumber = portNumber;
     }
 
+    /**
+     * Mark the current instance as the singleton instance for the class.
+     * @return self.
+     */
+    public GameServer setAsSingletonInstance(){
+        instance = this;
+        return this;
+    }
+
+    /**
+     * Singleton instance getter.
+     */
+    public static GameServer getInstance() {
+        return instance;
+    }
+
+    /**
+     * Start the server.
+     */
     public void startServer(){
 
         try{
             serverSocket = new ServerSocket(portNumber);
         } catch (IOException ex){
+            // Could not open the server socket (port not available)
             ex.printStackTrace();
             return;
         }
 
+        // Move accepting connections to a different thread.
         new Thread(() -> acceptIncomingConnections()).start();
     }
 
-    public void acceptIncomingConnections(){
+    /**
+     * Accept incoming connections.
+     */
+    private void acceptIncomingConnections(){
 
+        // Handle connections with custom threads
         ExecutorService connectionHandlers = Executors.newCachedThreadPool();
 
         while(true){
             try {
+                // Accept new connection and handle it
                 Socket socket = serverSocket.accept();
                 connectionHandlers.submit(new NewConnectionHandler(socket));
             } catch (IOException ex){
@@ -45,8 +80,38 @@ public class GameServer {
                 break;
             }
         }
-
+        // No need to keep this open anymore.
         connectionHandlers.shutdown();
+    }
+
+    /**
+     * Check whether a client with such id already exists.
+     */
+    public boolean idTaken(String id){
+        synchronized (clientMapLock){
+            return activeClients.containsKey(id);
+        }
+    }
+
+    /**
+     * Register a client using its id.
+     */
+    public void registerClient(ServerSideClient client){
+        synchronized (clientMapLock){
+            activeClients.put(client.getId(), client);
+        }
+    }
+
+    /**
+     * Retrieve a client using its id.
+     */
+    public ServerSideClient getClientById(String id) {
+
+        if(!activeClients.containsKey(id)) return null;
+
+        synchronized (clientMapLock) {
+            return activeClients.get(id);
+        }
     }
 
 }
