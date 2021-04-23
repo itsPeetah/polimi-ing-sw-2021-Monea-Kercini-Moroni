137e8678440290822d6a92ab81fd93ace6ba8f72
Giovanni Monea <giovanni.monea@epfl.ch>
2021-04-23 17:36:20 +0200

    Reworked EventHandler class

--- src/main/java/it/polimi/ingsw/model/events/EventHandler.java
+++ src/main/java/it/polimi/ingsw/model/events/EventHandler.java
@@ -4,70 +4,34 @@ import static it.polimi.ingsw.model.events.Action.*;
 
 /* CODE EXAMPLE FOR HANDLER USAGE:
 
-
-        // Get a reference to the singleton EventHandler
-        EventHandler eventHandler = EventHandler.getEventHandler();
-
         // Make a new request to the EventHandler for an action from a player
-        eventHandler.makeRequest(requestAction, requestPlayer);
+        EventHandler.makeRequest(requestAction, requestPlayer);
 
-        // Define local variables for needed data
-        Warehouse wh = new Warehouse();
+        // Ask for the result. IMPORTANT: this method is a BLOCKING method, meaning that it will stop the thread of the controller
+        YourEventData data = EventHandler.getResponse();
 
-        // Retrieve data in synchronization
-        synchronized(eventHandler) {
-            // Check if the player made a response
-            while(!eventHandler.isReady()) {
-                try {
-                    // If not, wait.
-                    eventHandler.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            // Retrieve data from the event
-            ParticularEventData data = (ParticularEventData)eventHandler.getData();
-
-            // Now, save the data to local variables
-            wh = data.getWarehouse();
-        }
-
-        // Now that data has been saved on local variables, exit the synchronized and do operations on data
-        wh.doWhateverYouWant;
+        // Now you can freely use the retrieved data (this is an example)
+        String stringRequested = data.useYourEventDataMethod();
 
      */
 
 public class EventHandler {
-    private static EventHandler eventHandler = null;
-    private Action requestAction;
-    private String requestPlayer;
-    private EventData responseData;
-    private Boolean ready;
-
-    private EventHandler() {
-        requestAction = NONE;
-        requestPlayer = null;
-        responseData = null;
-        ready = false;
-    }
-
-    /**
-     * Get a reference to the singleton EventHandler
-     */
-    public static EventHandler getEventHandler() {
-        if(eventHandler == null) eventHandler = new EventHandler();
-        return eventHandler;
-    }
+    private static final Object lock = new Object();
+    private static Action requestAction = NONE;
+    private static String requestPlayer = null;
+    private static EventData responseData = null;
+    private static Boolean ready = false;
 
     /**
      * Set the expected action and the player that must perform it.
      * @param requestAction action to be performed.
      * @param requestPlayer username of the player that must perform the action.
      */
-    public synchronized void makeRequest(Action requestAction, String requestPlayer) {
-        this.requestAction = requestAction;
-        this.requestPlayer = requestPlayer;
+    public static void makeRequest(Action requestAction, String requestPlayer) {
+        synchronized(lock) {
+            EventHandler.requestAction = requestAction;
+            EventHandler.requestPlayer = requestPlayer;
+        }
     }
 
     /**
@@ -76,28 +40,34 @@ public class EventHandler {
      * @param responsePlayer username of the player performing the response event.
      * @param responseData data of the response event.
      */
-    public synchronized void notify(Action responseAction, String responsePlayer, EventData responseData) {
-        // If the action is the expected one and the performing player is also the expected one, save the data and notify the controller.
-        if(responseAction == requestAction && responsePlayer.equals(requestPlayer)) {
-            this.responseData = responseData;
-            ready = true;
-            notifyAll();
+    public static void notify(Action responseAction, String responsePlayer, EventData responseData) {
+        synchronized(lock) {
+            // If the action is the expected one and the performing player is also the expected one, save the data and notify the controller.
+            if(responseAction == requestAction && responsePlayer.equals(requestPlayer)) {
+                EventHandler.responseData = responseData;
+                ready = true;
+                lock.notifyAll();
+            }
         }
     }
 
     /**
-     * Check if the requested event has been performed and data is ready to be retrieved.
+     * Get the response for the last requested action.
+     * @param <T> type of the requested data.
      */
-    public synchronized Boolean isReady() {
-        return ready;
+    public static <T extends EventData> T getResponse() {
+        synchronized(lock) {
+            // Check if the player generated a response
+            while (!ready) {
+                try {
+                    // If not, wait.
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // Once the response arrived, return it, casted to the correct type.
+            return (T)responseData;
+        }
     }
-
-    /**
-     * Get data from the last requested event.
-     */
-    public synchronized EventData getData() {
-        return responseData;
-    }
-
-
 }
