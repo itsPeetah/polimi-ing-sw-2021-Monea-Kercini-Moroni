f5ff78a632915fe0d7507381ec1bb402e83f7606
itsPeetah <pietro.g.moroni@gmail.com>
2021-05-07 15:34:11 +0200

    I missed this lmao

--- src/main/java/it/polimi/ingsw/network/server/components/RemoteConnectionHandler.java
+++ src/main/java/it/polimi/ingsw/network/server/components/RemoteConnectionHandler.java
@@ -58,7 +58,5 @@ public class RemoteConnectionHandler implements Runnable {
         else{
             user.terminateConnection();
         }
-
-        System.out.println(socket.getSocket().isClosed());
     }
 }
