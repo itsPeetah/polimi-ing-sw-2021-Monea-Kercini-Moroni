cfb3f583b2e190bba2545c904b8bbd6a05396d30
itsPeetah <pietro.g.moroni@gmail.com>
2021-05-07 15:15:39 +0200

    Added "QUIT" message and new statics

--- src/main/java/it/polimi/ingsw/network/common/ConnectionMessage.java
+++ src/main/java/it/polimi/ingsw/network/common/ConnectionMessage.java
@@ -1,16 +1,20 @@
 package it.polimi.ingsw.network.common;
 
-public enum ConnectionMessage {
+public enum ConnectionMessage implements IMessageClass {
     WELCOME("WELCOME"),
     HELLO("HELLO"),
     ASSIGNID("ID"),
     READY("READY"),
+    QUIT("QUIT"),
     OK("OK"),
     ERR("ERR");
 
     public static final String welcomeMessage = WELCOME.addBody("Welcome to the server!");
-    public static final String unexpectedReplyError = ERR.addBody("Connection refused: unexpected reply from client.");
     public static final String connectionReadyMessage = READY.addBody("You are now connected to the server!");
+
+    public static final String unexpectedReplyError = ERR.addBody("Connection refused: unexpected reply from client.");
+    public static final String missingArgumentsError = ERR.addBody("Invalid request: missing arguments.");
+    public static final String invalidRequestError = ERR.addBody("Invalid request: the request was not valid.");
 
     private final String messageCode;
 
