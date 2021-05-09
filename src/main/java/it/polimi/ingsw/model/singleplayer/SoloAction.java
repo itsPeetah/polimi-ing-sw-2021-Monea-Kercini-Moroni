bf1bc1f0b2669fb4266e7cb57df2dfa4f73b5c38
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-05-09 23:03:46 +0200

    Finished SoloAction class

--- src/main/java/it/polimi/ingsw/model/singleplayer/SoloAction.java
+++ src/main/java/it/polimi/ingsw/model/singleplayer/SoloAction.java
@@ -1,12 +1,16 @@
 package it.polimi.ingsw.model.singleplayer;
 
+import it.polimi.ingsw.model.game.DevCardMarket;
+import it.polimi.ingsw.model.general.Color;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
-public final class SoloAction {
+public class SoloAction {
 
     private ArrayList<SoloActionTokens> soloActionTokens = new ArrayList<SoloActionTokens>();
-    int difficulty;
+    private int difficulty;
+    private BlackCross cross;
 
     /**
      * Prepares Lorenzo solo action tokens for single player
@@ -34,6 +38,39 @@ public final class SoloAction {
         this.difficulty = difficulty;
     }
 
+    /**
+     * Simulates Lorenzos turn in a single player turn
+     * @param dcm the DevCardMarket Lorenzo applies his actions
+     * @return true if Lorenzo has won the turn
+     */
+    public boolean playLorenzoTurn(DevCardMarket dcm){
 
+        boolean win = false;
+
+        switch (soloActionTokens.get(0)){
+
+            case DISCARD_2_BLUE:
+                win = dcm.discard2(Color.BLUE);
+                break;
+            case DISCARD_2_GREEN:
+                win = dcm.discard2(Color.GREEN);
+                break;
+            case DISCARD_2_PURPLE:
+                win = dcm.discard2(Color.PURPLE);
+                break;
+            case DISCARD_2_YELLOW:
+                win = dcm.discard2(Color.YELLOW);
+                break;
+            case MOVE_2:
+                win = cross.incrementBlackFaith(2);
+                break;
+            case MOVE_1_SHUFFLE:
+                win = cross.incrementBlackFaith(1);
+                //Recreate shuffled deck
+                soloActionTokens = createSoloActionDeck();
+                break;
+        }
+        return win;
+    }
 
 }
