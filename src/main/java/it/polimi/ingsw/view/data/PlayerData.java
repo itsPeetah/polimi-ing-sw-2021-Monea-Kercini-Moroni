c97f52cfca50941f7b7daab373521e13738e5a1d
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-05-12 19:02:02 +0200

    PlayerData setup

--- src/main/java/it/polimi/ingsw/view/data/PlayerData.java
+++ src/main/java/it/polimi/ingsw/view/data/PlayerData.java
@@ -1,4 +1,31 @@
 package it.polimi.ingsw.view.data;
 
+import it.polimi.ingsw.view.data.player.*;
+
 public class PlayerData {
+    private DevCards devCards;
+    private FaithTrack faithTrack;
+    private PlayerLeaders playerLeaders;
+    private Warehouse warehouse;
+    private Strongbox strongbox;
+
+    public DevCards getDevCards() {
+        return devCards;
+    }
+
+    public FaithTrack getFaithTrack() {
+        return faithTrack;
+    }
+
+    public PlayerLeaders getPlayerLeaders() {
+        return playerLeaders;
+    }
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
+    public Strongbox getStrongbox() {
+        return strongbox;
+    }
 }
