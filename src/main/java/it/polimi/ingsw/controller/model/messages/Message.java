39fe1ace30f19978bb3bfd5d64ffea1e815d7a7d
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-05-12 12:13:49 +0200

    Added Ok and Warehouse not organized messages

--- src/main/java/it/polimi/ingsw/controller/model/messages/Message.java
+++ src/main/java/it/polimi/ingsw/controller/model/messages/Message.java
@@ -2,11 +2,13 @@ package it.polimi.ingsw.controller.model.messages;
 
 public enum Message {
     ERROR("ERROR"),
+    OK("Action executed correctly."),
     START_TURN("It's your turn!"),
     NOT_ENOUGH_RESOURCES("You don't have enough resources."),
     ILLEGAL_CARD_PLACE("You can't put the card there."),
     REQUIREMENTS_NOT_MET("You don't meet the requirements to play this card."),
     ALREADY_USED_PRIMARY_ACTION("You have already used your primary action."),
+    WAREHOUSE_UNORGANIZED("Your warehouse is not organized."),
     WINNER("VICTORY!"),
     LOSER("WASTED! You were no match for Lorenzo's magnificence!");
 
