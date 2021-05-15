e1401b86c7701ad367676c3031835884c247aa3a
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-05-15 16:04:12 +0200

    Created MomentaryData

---
+++ src/main/java/it/polimi/ingsw/view/data/MomentaryData.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.view.data;
+
+import it.polimi.ingsw.view.data.momentary.LeadersToChooseFrom;
+import it.polimi.ingsw.view.data.momentary.ResourcesToPut;
+
+
+public class MomentaryData {
+
+    private LeadersToChooseFrom leaders;
+    private ResourcesToPut res;
+
+    public MomentaryData() {
+        leaders = new LeadersToChooseFrom();
+        res = new ResourcesToPut();
+
+    }
+
+    public LeadersToChooseFrom getLeaders() {
+        return leaders;
+    }
+
+    public ResourcesToPut getRes() {
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return "Momentary Data{" +
+                "leaders=" + leaders.toString() +
+                ", resources=" + res.toString() +
+                '}';
+    }
+}
