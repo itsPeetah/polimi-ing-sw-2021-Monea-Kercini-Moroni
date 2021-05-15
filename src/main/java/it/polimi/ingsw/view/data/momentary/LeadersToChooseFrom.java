0fe58901f713c75b23f3377e9a89248dafdac250
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-05-15 15:56:10 +0200

    Created resources to put in player data and fixed both classes for observability

--- src/main/java/it/polimi/ingsw/view/data/momentary/LeadersToChooseFrom.java
+++ src/main/java/it/polimi/ingsw/view/data/momentary/LeadersToChooseFrom.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.view.data.momentary;
 
 import it.polimi.ingsw.model.cards.LeadCard;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 
@@ -11,10 +12,12 @@ public class LeadersToChooseFrom extends Observable {
 
     public synchronized void setLeaders(List<LeadCard> leaders){
         this.leaders = leaders;
+        setChanged();
+        notifyObservers(leaders);
     }
 
-    public LeadersToChooseFrom(List<LeadCard> leaders) {
-        this.leaders = leaders;
+    public LeadersToChooseFrom() {
+        this.leaders = new ArrayList<>();
     }
 
 }
