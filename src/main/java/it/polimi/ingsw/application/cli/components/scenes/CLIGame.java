538ce0a499fbd825dd717300c505910fd68e4bb2
itsPeetah <pietro.g.moroni@gmail.com>
2021-05-17 11:23:46 +0200

    Started adding CLI game scenes

--- src/main/java/it/polimi/ingsw/application/cli/components/scenes/CLIGame.java
+++ src/main/java/it/polimi/ingsw/application/cli/components/scenes/CLIGame.java
@@ -1,11 +1,28 @@
 package it.polimi.ingsw.application.cli.components.scenes;
 
 import it.polimi.ingsw.application.cli.components.CLIScene;
+import it.polimi.ingsw.application.cli.components.scenes.game.*;
+import it.polimi.ingsw.controller.view.game.GameController;
 
 public class CLIGame extends CLIScene {
 
+    private CLIBoard board;
+    private CLIDevCardMarket devCardMarket;
+    private CLIFaithTrack faithTrack;
+    private CLILeadChoice leadChoice;
+    private CLIResourceChoice resourceChoice;
+    private CLIResourceMarket resourceMarket;
+
+
     public CLIGame() {
         super();
+        this.board = new CLIBoard();
+        this.devCardMarket = new CLIDevCardMarket();
+        this.faithTrack = new CLIFaithTrack();
+        this.leadChoice = new CLILeadChoice();
+        this.resourceChoice = new CLIResourceChoice();
+        this.resourceMarket = new CLIResourceMarket();
+
     }
 
     @Override
@@ -16,4 +33,6 @@ public class CLIGame extends CLIScene {
 
         print("====================================================");
     }
+
+
 }
