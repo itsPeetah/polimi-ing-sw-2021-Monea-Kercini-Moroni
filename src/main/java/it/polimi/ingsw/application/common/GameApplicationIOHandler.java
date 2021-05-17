30754f589b2726854200835a7fe767e48c991a4c
itsPeetah <pietro.g.moroni@gmail.com>
2021-05-17 11:40:42 +0200

    Implemented JSONUtility class where needed

--- src/main/java/it/polimi/ingsw/application/common/GameApplicationIOHandler.java
+++ src/main/java/it/polimi/ingsw/application/common/GameApplicationIOHandler.java
@@ -7,12 +7,12 @@ import it.polimi.ingsw.controller.model.updates.UpdatePacket;
 import it.polimi.ingsw.network.common.NetworkPacket;
 import it.polimi.ingsw.network.common.sysmsg.ConnectionMessage;
 import it.polimi.ingsw.network.common.sysmsg.GameLobbyMessage;
+import it.polimi.ingsw.util.JSONUtility;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class GameApplicationIOHandler {
-    protected final static Gson gson = new Gson();
 
     // TODO Add Thread Pool for elaborating commands
 
@@ -26,12 +26,12 @@ public class GameApplicationIOHandler {
     }
 
     public void notifyMessage(NetworkPacket messageNetworkPacket) {
-        MessagePacket messagePacket = gson.fromJson(messageNetworkPacket.getPayload(), MessagePacket.class);
+        MessagePacket messagePacket = JSONUtility.fromJson(messageNetworkPacket.getPayload(), MessagePacket.class);
         GameApplication.getInstance().getGameControllerIO().notifyMessage(messagePacket);
     }
 
     public void notifyUpdate(NetworkPacket updateNetworkPacket) {
-        UpdatePacket updatePacket = gson.fromJson(updateNetworkPacket.getPayload(), UpdatePacket.class);
+        UpdatePacket updatePacket = JSONUtility.fromJson(updateNetworkPacket.getPayload(), UpdatePacket.class);
         GameApplication.getInstance().getGameControllerIO().notifyUpdate(updatePacket);
     }
 
