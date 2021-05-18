beb47b024613c7f4309229675795303d59ad1722
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-05-18 16:11:44 +0200

    Fixed bug in game title

--- src/main/java/it/polimi/ingsw/application/gui/scenes/GUIMPRoom.java
+++ src/main/java/it/polimi/ingsw/application/gui/scenes/GUIMPRoom.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class GUIMPRoom implements Initializable {
+
     public static ObservableList<String> observablePlayersList = FXCollections.observableArrayList();
 
     @FXML
