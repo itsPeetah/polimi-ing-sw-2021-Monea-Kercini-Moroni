c429597f7028024766ce4ee19719997dc6890f1e
itsPeetah <pietro.g.moroni@gmail.com>
2021-05-19 09:22:37 +0200

    Started implementing LeaderChoice

--- src/main/java/it/polimi/ingsw/application/cli/components/scenes/game/CLILeadChoice.java
+++ src/main/java/it/polimi/ingsw/application/cli/components/scenes/game/CLILeadChoice.java
@@ -2,15 +2,27 @@ package it.polimi.ingsw.application.cli.components.scenes.game;
 
 import it.polimi.ingsw.application.cli.components.CLIScene;
 import it.polimi.ingsw.view.data.GameData;
+import it.polimi.ingsw.view.data.momentary.LeadersToChooseFrom;
 
 public class CLILeadChoice extends CLIScene implements ICLIGameSubScene {
+
+    LeadersToChooseFrom leadersToChooseFrom;
+
     public CLILeadChoice()
     {
         super();
+        leadersToChooseFrom = null;
     }
 
     @Override
     public void update(GameData data) {
+        leadersToChooseFrom = data.getMomentary().getLeaders();
+    }
 
+    @Override
+    public void show() {
+        print("LEADERS TO CHOOSE FROM:"); print("");
+        print(leadersToChooseFrom.toString());
+        print("");
     }
 }
