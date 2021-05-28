b6ce3e866a09f1f09640b0322b0f47027c38d53b
itsPeetah <pietro.g.moroni@gmail.com>
2021-05-28 15:07:03 +0200

    Minor formatting change

--- src/main/java/it/polimi/ingsw/application/cli/util/ANSIColor.java
+++ src/main/java/it/polimi/ingsw/application/cli/util/ANSIColor.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.application.cli.util;
 
 public class ANSIColor {
     public static final String RESET = "\u001B[0m";
+
     public static final String BLACK = "\u001B[30m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
