7495ec088911c2d2c47a897e5bb064e15090d61c
giommok <giovanni.monea@epfl.ch>
2021-06-07 20:14:06 +0200

    Added annulable tag to additional content parameter in system message listener

--- src/main/java/it/polimi/ingsw/application/common/listeners/SystemMessageListener.java
+++ src/main/java/it/polimi/ingsw/application/common/listeners/SystemMessageListener.java
@@ -1,9 +1,10 @@
 package it.polimi.ingsw.application.common.listeners;
 
 import it.polimi.ingsw.network.common.SystemMessage;
+import org.jetbrains.annotations.Nullable;
 
 public interface SystemMessageListener {
 
-    void onSystemMessage(SystemMessage type, String additionalContent);
+    void onSystemMessage(SystemMessage type, @Nullable String additionalContent);
 
 }
