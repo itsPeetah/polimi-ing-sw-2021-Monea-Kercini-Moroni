7d99b18a6accf8d15e12ac0618a88eabe08dd55a
giommok <giovanni.monea@epfl.ch>
2021-06-18 01:30:59 +0200

    Changed message text

--- src/main/java/it/polimi/ingsw/controller/model/messages/Message.java
+++ src/main/java/it/polimi/ingsw/controller/model/messages/Message.java
@@ -2,10 +2,10 @@ package it.polimi.ingsw.controller.model.messages;
 
 public enum Message {
     ERROR("ERROR"),
-    OK("Action executed correctly."),
+    OK("Action executed! Choose your next move!"),
     SETTING_UP_GAME("The players are making their choices, please wait for your turn"),
     GAME_HAS_STARTED("The game has begun!"),
-    TURN_PASSED("A player has ended his turn"),
+    TURN_PASSED("A player has ended their turn"),
     CHOOSE_LEADERS("Choose your 2 leaders to keep in your hand"),
     CHOOSE_RESOURCE("Please choose a resource of your choice"),
     CHOOSE_REPLACEMENT("Please choose a resource of your choice"),
@@ -15,7 +15,7 @@ public enum Message {
     ILLEGAL_CARD_PLACE("You can't put the card there"),
     REQUIREMENTS_NOT_MET("You don't meet the requirements to play this card"),
     ALREADY_USED_PRIMARY_ACTION("You have already used your primary action"),
-    WAREHOUSE_UNORGANIZED("Please organize your warehouse"),
+    WAREHOUSE_UNORGANIZED("Please, organize your warehouse"),
     WINNER("VICTORY!"),
     LOSER("WASTED! You were no match for Lorenzo's magnificence!"),
     LOSER_MULTIPLAYER("WASTED!"),
