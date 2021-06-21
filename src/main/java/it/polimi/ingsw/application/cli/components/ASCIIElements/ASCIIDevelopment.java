4bef7a9435696f0c33e1bb87447275896ffd54f7
itsPeetah <pietro.g.moroni@gmail.com>
2021-06-21 09:04:14 +0200

    Added default production to ASCIIDevelopment

--- src/main/java/it/polimi/ingsw/application/cli/components/ASCIIElements/ASCIIDevelopment.java
+++ src/main/java/it/polimi/ingsw/application/cli/components/ASCIIElements/ASCIIDevelopment.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.application.cli.components.ASCIIElements;
 
 import it.polimi.ingsw.application.common.GameApplication;
 import it.polimi.ingsw.model.cards.DevCard;
+import it.polimi.ingsw.model.general.Production;
+import it.polimi.ingsw.model.playerboard.ProductionPowers;
 import it.polimi.ingsw.view.data.GameData;
 
 public class ASCIIDevelopment {
@@ -10,6 +12,8 @@ public class ASCIIDevelopment {
         DevCard[] dcs = GameApplication.getInstance().getGameController().getGameData().getPlayerData(player).getDevCards().getDevCards();
 
         System.out.println(player + "'s productions:");
+        System.out.print("Default production: ");
+        ASCIIProduction.draw(ProductionPowers.getBasicProduction());
         for(int i = 0; i < dcs.length; i++){
             System.out.print("Stack #" + i + ": ");
             if(dcs[i] != null) ASCIIProduction.draw(dcs[i].getProduction());
