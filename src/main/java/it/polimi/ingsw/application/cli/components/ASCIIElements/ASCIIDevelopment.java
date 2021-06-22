7d754392bd453b6f0420ce80d0db78f41cdde950
itsPeetah <pietro.g.moroni@gmail.com>
2021-06-22 10:41:29 +0200

    CLI Updates

--- src/main/java/it/polimi/ingsw/application/cli/components/ASCIIElements/ASCIIDevelopment.java
+++ src/main/java/it/polimi/ingsw/application/cli/components/ASCIIElements/ASCIIDevelopment.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.application.cli.components.ASCIIElements;
 
+import it.polimi.ingsw.application.cli.util.ANSIColor;
 import it.polimi.ingsw.application.common.GameApplication;
 import it.polimi.ingsw.model.cards.DevCard;
 import it.polimi.ingsw.model.cards.LeadCard;
+import it.polimi.ingsw.model.general.Color;
 import it.polimi.ingsw.model.general.Production;
 import it.polimi.ingsw.model.playerboard.ProductionPowers;
 import it.polimi.ingsw.model.playerleaders.CardState;
@@ -17,12 +19,28 @@ public class ASCIIDevelopment {
         PlayerLeaders pls = gd.getPlayerData(player).getPlayerLeaders();
 
         System.out.println(player + "'s productions:");
-        System.out.print("Default production: ");
+        /*for(DevCard dc : dcs){
+            if(dc==null) continue;
+            String color = dc.getColor() == Color.BLUE ? ANSIColor.BLUE :
+                    dc.getColor() == Color.GREEN ? ANSIColor.GREEN :
+                            dc.getColor() == Color.YELLOW ? ANSIColor.YELLOW :
+                                    dc.getColor() == Color.PURPLE ? ANSIColor.PURPLE : ANSIColor.RESET;
+            System.out.println("> " + color + dc.getColor().toString() + " " + dc.getLevel().toString() +ANSIColor.RESET + " (VP: " + dc.getVictoryPoints() +")");
+        }*/
+        System.out.println("Productions:");
+        System.out.print("(0) Default production: ");
         ASCIIProduction.draw(ProductionPowers.getBasicProduction());
         for(int i = 0; i < dcs.length; i++){
-            System.out.print("Stack #" + (i+1) + ": ");
-            if(dcs[i] != null) ASCIIProduction.draw(dcs[i].getProduction());
-            else System.out.print("empty\n");
+            System.out.print("("+ (i+1) +") "+"Stack #" + (i+1));
+            if(dcs[i] != null) {
+                String color = dcs[i].getColor() == Color.BLUE ? ANSIColor.BLUE :
+                        dcs[i].getColor() == Color.GREEN ? ANSIColor.GREEN :
+                                dcs[i].getColor() == Color.YELLOW ? ANSIColor.YELLOW :
+                                        dcs[i].getColor() == Color.PURPLE ? ANSIColor.PURPLE : ANSIColor.RESET;
+                System.out.print(" " + color + dcs[i].getColor().toString() + " " + dcs[i].getLevel().toString() +ANSIColor.RESET + " (VP: " + dcs[i].getVictoryPoints() +"): ");
+                ASCIIProduction.draw(dcs[i].getProduction());
+            }
+            else System.out.print(": empty\n");
         }
         for(int i = 0; i < 2; i++){
             if(pls.getStates()[i] == CardState.PLAYED && pls.getLeaders()[i].getAbility().getProduction() != null) {
@@ -31,6 +49,7 @@ public class ASCIIDevelopment {
             }
         }
         System.out.println("____________________");
+
 
     }
 }
--- src/main/java/it/polimi/ingsw/application/cli/components/scenes/game/CLIBoard.java
+++ src/main/java/it/polimi/ingsw/application/cli/components/scenes/game/CLIBoard.java
@@ -45,13 +45,14 @@ public class CLIBoard extends CLIScene implements CLIGameSubScene {
         println("\tExample usage: \"produce 1 4\" to only activate the productions in the first stack and the first active leader.");
         println("Use \"activate <1|2>\" to activate a leader.");
         println("Use \"discard <1|2>\" to discard a leader.");
-        println("User \"endturn\" to end your turn.");
+        println("User \"end\" to end your turn.");
     }
 
     @Override
     public void execute(String command, String[] arguments) {
         if ((arguments == null || arguments.length < 1) && ( !"end endturn help".contains(command))) {
             error("Missing arguments.");
+            return;
         }
 
         switch (command) {
@@ -74,7 +75,6 @@ public class CLIBoard extends CLIScene implements CLIGameSubScene {
                 onPlayLeader(arguments, false);
                 break;
             case "end":
-            case "endturn":
                 onEndTurn();
                 break;
             case "help":
