de3e9e3dcd13268a99f6b27054635f679273ef5c
MarvinKercini <79702661+MarvinKercini@users.noreply.github.com>
2021-06-25 23:04:06 +0200

    Changed from file to input stream in GUI application

--- src/main/java/it/polimi/ingsw/application/gui/GUIApplication.java
+++ src/main/java/it/polimi/ingsw/application/gui/GUIApplication.java
@@ -1,12 +1,11 @@
 package it.polimi.ingsw.application.gui;
 
-import it.polimi.ingsw.application.common.GameApplication;
 import it.polimi.ingsw.application.gui.scenes.GUIMainGame;
 import it.polimi.ingsw.model.cards.CardManager;
 import it.polimi.ingsw.model.singleplayer.SoloActionTokens;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
@@ -14,10 +13,12 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.InputStream;
+
 
 public class GUIApplication extends Application {
     private static Alert oldDialog;
-    public static final String ICON_PATH = "src/main/resources/images/other/calamaio.png";
+    public static final String ICON_PATH = "images/other/calamaio.png";
     private static final String BLACK_LADDER_FONT_PATH = "fonts/BITCBLKAD.ttf";
     private static final String HIGH_TOWER_FONT_PATH = "fonts/HTOWERT.TTF";
 
@@ -35,8 +36,11 @@ public class GUIApplication extends Application {
         stage.setScene(GUIScene.getScene());
         GUIScene.MAIN_MENU.load();
         stage.setResizable(false);
-        File file = new File(ICON_PATH);
-        Image iconImage = new Image(file.toURI().toString());
+
+        //trying with input stream
+        InputStream is = GUIApplication.class.getClassLoader().getResourceAsStream(ICON_PATH);
+
+        Image iconImage = new Image(is);
         stage.getIcons().add(iconImage);
         stage.setOnCloseRequest(windowEvent -> {
             Platform.exit();
